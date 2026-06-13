7e003bd8a1c181a250a3504137faec55ac42e9bc
annacitrus <aecitron@icloud.com>
2026-06-13 17:52:26 -0400

    Initial commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+interface ListGroupProps {
+    items: string[];
+    heading: string;
+    courses: { [key: string]: { name: string; credits: number; related?: string[] } };
+    onSelectItem: (item:string) => void;
+}
+
+function ListGroup({items, heading, onSelectItem}: ListGroupProps) {
+
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event)
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p> No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            key={item}
+            onClick = {() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
