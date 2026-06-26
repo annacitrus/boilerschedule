f605863d76ef9e67e87d87271208afdc89f22230
annacitrus <aecitron@icloud.com>
2026-06-26 18:14:31 -0400

    add ibe

--- src/data/integratedBusinessEngineering.ts
+++ src/data/integratedBusinessEngineering.ts
@@ -7,7 +7,7 @@ const integratedBusinessEngineering: MajorData = {
     ACCT20000: { name: "Introductory Accounting", credits: 3 },
     IBE29110: { name: "Integrated Business & Engineering Seminar I", credits: 1 },
     CHM11510: { name: "General Chemistry I", credits: 3 },
-    "Chemistry Lab": { name: "General Chemistry I Laboratory", credits: 1 },
+    CHM11520: { name: "General Chemistry I Laboratory (or 11530)", credits: 1 },
     MA16500: { name: "Analytic Geometry And Calculus I", credits: 4 },
     "EPICS/VIP I": { name: "EPICS/VIP I", credits: 1 },
 
@@ -22,7 +22,7 @@ const integratedBusinessEngineering: MajorData = {
     // Fall 2nd Year
     ACCT20100: { name: "Management Accounting I", credits: 3, prereqs: ["ACCT20000"] },
     IBE29120: { name: "Integrated Business & Engineering Seminar II", credits: 1 },
-    IBE29150a: { name: "IBE Professional Development", credits: 0 },
+    IBE29150: { name: "IBE Professional Development", credits: 0 },
     MA26100: { name: "Multivariate Calculus", credits: 4, prereqs: ["MA16600", "MA16500"] },
     STAT35000: { name: "Introduction To Statistics", credits: 3, prereqs: ["MA16600", "MA16500"] },
     "Written Communication Selective": { name: "Written Communication Selective", credits: 3 },
@@ -31,22 +31,19 @@ const integratedBusinessEngineering: MajorData = {
     // Spring 2nd Year
     FIN31000: { name: "Financial Management", credits: 3, prereqs: ["ACCT20000", "ACCT20100"] },
     IBE29130: { name: "Integrated Business & Engineering Seminar III", credits: 1 },
-    IBE29150b: { name: "IBE Professional Development", credits: 0 },
-    MIS38800: { name: "Python For Business", credits: 3 },
+    MIS38800: { name: "Python For Business", credits: 3, prereqs: ["ENGR13300"]},
     MKTG32400: { name: "Marketing Management", credits: 3 },
     QM30500: { name: "Business Statistics", credits: 3, prereqs: ["STAT35000", "MA16600", "MA16500"] },
     "Engineering IBE Core II": { name: "Engineering IBE Core Selective", credits: 3 },
 
     // Fall 3rd Year
-    IBE29150c: { name: "IBE Professional Development", credits: 0 },
-    SCOM36100: { name: "Operations Management", credits: 3, prereqs: ["QM30500", "STAT35000", "MA16600", "MA16500"] },
+    SCOM36100: { name: "Operations Management", credits: 3, prereqs: ["STAT35000", "MA16600", "MA16500"] },
     STRT49200: { name: "Intellectual Property Management", credits: 2 },
     "Business Method Selective I": { name: "Business Method Selective", credits: 3 },
     "Specialties Course I": { name: "Specialties Course", credits: 3 },
     "Team Based Design Selective I": { name: "Team Based Design Selective", credits: 2 },
 
     // Spring 3rd Year
-    IBE29150d: { name: "IBE Professional Development", credits: 0 },
     MIS38200: { name: "Management Information Systems", credits: 3 },
     "Business Method Selective II": { name: "Business Method Selective", credits: 3 },
     "Engineering Expansion Course": { name: "Engineering Expansion Course", credits: 3 },
@@ -57,7 +54,7 @@ const integratedBusinessEngineering: MajorData = {
     IBE29140: { name: "Integrated Business & Engineering Seminar IV", credits: 1 },
     OBHR33000: { name: "Introduction To Organizational Behavior", credits: 3 },
     OBHR49300: { name: "Storytelling With Data", credits: 2 },
-    STRT35200: { name: "Strategic Management", credits: 3 },
+    STRT35200: { name: "Strategic Management", credits: 3, prereqs: ["ECON25100"] },
     "Specialties Course II": { name: "Specialties Course", credits: 3 },
     "Human Cultures Humanities": { name: "Human Cultures: Humanities", credits: 3 },
 
@@ -71,7 +68,7 @@ const integratedBusinessEngineering: MajorData = {
   semesters: [
     {
       label: "Fall 1st Year",
-      courses: ["ENGR13300", "ACCT20000", "IBE29110", "CHM11510", "Chemistry Lab", "MA16500", "EPICS/VIP I"],
+      courses: ["ENGR13300", "ACCT20000", "IBE29110", "CHM11510", "CHM11520", "MA16500", "EPICS/VIP I"],
     },
     {
       label: "Spring 1st Year",
@@ -79,19 +76,19 @@ const integratedBusinessEngineering: MajorData = {
     },
     {
       label: "Fall 2nd Year",
-      courses: ["ACCT20100", "IBE29120", "IBE29150a", "MA26100", "STAT35000", "Written Communication Selective", "Engineering IBE Core I"],
+      courses: ["ACCT20100", "IBE29120", "IBE29150", "MA26100", "STAT35000", "Written Communication Selective", "Engineering IBE Core I"],
     },
     {
       label: "Spring 2nd Year",
-      courses: ["FIN31000", "IBE29130", "IBE29150b", "MIS38800", "MKTG32400", "QM30500", "Engineering IBE Core II"],
+      courses: ["FIN31000", "IBE29130", "IBE29150", "MIS38800", "MKTG32400", "QM30500", "Engineering IBE Core II"],
     },
     {
       label: "Fall 3rd Year",
-      courses: ["IBE29150c", "SCOM36100", "STRT49200", "Business Method Selective I", "Specialties Course I", "Team Based Design Selective I"],
+      courses: ["IBE29150", "SCOM36100", "STRT49200", "Business Method Selective I", "Specialties Course I", "Team Based Design Selective I"],
     },
     {
       label: "Spring 3rd Year",
-      courses: ["IBE29150d", "MIS38200", "Business Method Selective II", "Engineering Expansion Course", "Engineering Selective", "Team Based Design Selective II"],
+      courses: ["IBE29150", "MIS38200", "Business Method Selective II", "Engineering Expansion Course", "Engineering Selective", "Team Based Design Selective II"],
     },
     {
       label: "Fall 4th Year",
